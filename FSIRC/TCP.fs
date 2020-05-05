9ef4e4656b8eeb9d3df5ed3d712d047239a56e2c
FatherFoxxy <rattusunison@gmail.com>
2020-05-05 15:31:25 -0700

    Simplify TCP functions, remove read as the implementation was incorrect anyway, use task over async

--- FSIRC/TCP.fs
+++ FSIRC/TCP.fs
@@ -1,11 +1,11 @@
 module FSIRC.TCP
 
-open Extensions
-
 open System
 open System.Text
 open System.Net.Sockets
 open System.IO
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open System.Threading.Tasks
 
 type ConnectionDetails =
     {
@@ -13,13 +13,12 @@ type ConnectionDetails =
         port : int
     }
 
-let connect (tcpClient : TcpClient) connectionDetails = async {
-    return
+let connect (tcpClient : TcpClient) connectionDetails = task {
+    return!
         tcpClient.ConnectAsync(connectionDetails.host, connectionDetails.port)
-        |> Async.awaitTaskResult
 }
 
-let stream (tcpClient : TcpClient) = tcpClient.GetStream()
+let getStream (tcpClient : TcpClient) = tcpClient.GetStream()
 
 let encodeStr (str : string) =
     Encoding.ASCII.GetBytes(str)
@@ -27,32 +26,6 @@ let encodeStr (str : string) =
 let decodeStr (bytes : byte[]) =
     Encoding.ASCII.GetString(bytes)
 
-let read (tcpClient : TcpClient) = async {
-    let stream = stream tcpClient
-
-    let rec readNext data =
-        if not stream.DataAvailable then
-            data
-        else
-            let buffer = Array.create 2048 0uy
-            let numRead = stream.Read(buffer, 0, buffer.Length)
-            if numRead < 2048 then
-                readNext (buffer.[0..numRead-1] :: data)
-            else
-                readNext (buffer :: data)
-
-    return
-        readNext []
-        |> List.rev
-        |> Array.concat
+let write (stream: NetworkStream) data = task {
+    return! stream.WriteAsync(data, 0, data.Length)
 }
-
-let write (tcpClient : TcpClient) data = async {
-    let stream = stream tcpClient
-    let res =
-        stream.WriteAsync(data, 0, data.Length)
-        |> Async.awaitTaskResult
-    return res
-}
-
-
